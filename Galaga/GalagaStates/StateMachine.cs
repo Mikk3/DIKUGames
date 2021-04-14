3353698e995750d929c3f786a209a054d093286b
Mikkel <4072916+Mikk3@users.noreply.github.com>
2021-04-14 17:07:23 +0200

    Removed unnecessary event subscription

--- Galaga/GalagaStates/StateMachine.cs
+++ Galaga/GalagaStates/StateMachine.cs
@@ -7,15 +7,12 @@ namespace Galaga.GalagaStates {
     public class StateMachine : IGameEventProcessor<object> {
         public IGameState ActiveState { get; private set; }
         public StateMachine() {
-            GalagaBus.GetBus().Subscribe(GameEventType.GameStateEvent, this);
-            GalagaBus.GetBus().Subscribe(GameEventType.InputEvent, this);
-
             ActiveState = MainMenu.GetInstance();
         }
 
-        
+
         private void SwitchState(GameStateType stateType) {
-            switch (stateType) { 
+            switch (stateType) {
 
                 case GameStateType.GameRunning:
                     ActiveState = GameRunning.GetInstance();
